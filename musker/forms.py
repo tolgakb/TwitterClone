5b75b6848d1f42a4f1f83f84029e9c341bfabf8f
tolgakb <tolgakabaahmet@gmail.com>
2023-07-27 16:18:50 +0100

    Profile picture has been added to home, profile_list and profile page and users are able to upload their profil picture on profile page.

--- musker/forms.py
+++ musker/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import Tweet
+from .models import Tweet, Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
+#Profile Extras Form
+class ProfilePicForm(forms.ModelForm):
+    profile_image = forms.ImageField(label="Profile Picture")
+
+    class Meta:
+        model = Profile
+        fields = ('profile_image', )
 
 class TweetForm(forms.ModelForm):
     body = forms.CharField(required= True,
--- musker/views.py
+++ musker/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect, render
 from .models import Profile, Tweet
 from django.contrib import messages
-from .forms import TweetForm, SignUpForm
+from .forms import TweetForm, SignUpForm, ProfilePicForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
@@ -18,9 +20,9 @@ def home(request):
                 tweet.save()
                 messages.success(request, "Your tweet has been posted!")
                 return redirect("home")
-            
+                
         tweets = Tweet.objects.all().order_by("-created_at")
-        return render(request, 'home.html', {"tweets": tweets, "form": form})
+        return render(request, 'home.html', {"tweets": tweets, "form": form ,})
     else:
         tweets = Tweet.objects.all().order_by("-created_at")
         return render(request, 'home.html', {"tweets": tweets})
@@ -97,3 +99,26 @@ def register_user(request):
             return redirect('home')
 
     return render(request, "register.html", {"form": form})
+
+def update_user(request):
+
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id = request.user.id)
+        profile_user = Profile.objects.get(user__id = request.user.id)
+
+        #Get Forms
+        user_form = SignUpForm(request.POST or None, request.FILES or None, instance = current_user)
+        profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance = profile_user)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            login(request, current_user)
+            messages.success(request, "Your profile has been updated. ")
+            return redirect('home')
+
+        return render(request, "update_user.html", {'user_form': user_form, 'profile_form': profile_form})
+    
+    else:
+        messages.success(request, ("You must be logged in to view this page."))
+        return redirect('home')
+
